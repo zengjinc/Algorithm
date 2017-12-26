28583b6bb1649e9b0b555c383c9276b214c48b9c
zengjinc <102985588@qq.com>
2017-12-26 14:22:38 +0800

    update

--- src/test/algorithm/Test.java
+++ src/test/algorithm/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("main method of test class");
 	}
-	public void method1(){
-		System.out.println("method1 of test class");
-	}
+//	public void method1(){
+//		System.out.println("method1 of test class");
+//	}
 }
