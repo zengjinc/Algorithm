f3aa6dbb7f804f0732b540a8b254a785bdea3431
zengjinc <102985588@qq.com>
2017-12-26 14:19:37 +0800

    create

--- src/test/algorithm/Test.java
+++ src/test/algorithm/Test.java
@@ -6,7 +6,13 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("main method of test class");
 	}
+	
 	public void method1(){
 		System.out.println("method1 of test class");
 	}
+	
+	public void method2(){
+		System.out.println("methdo2 of test class");
+	}
+	
 }
