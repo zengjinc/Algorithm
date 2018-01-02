87dcb85577fdeaef5f6c156cf5e336e5a7d1446c
zengjinc <102985588@qq.com>
2018-01-02 15:37:39 +0800

    update Test,add a line sysout

--- src/test/algorithm/Test.java
+++ src/test/algorithm/Test.java
@@ -1,9 +1,21 @@
 package test.algorithm;
 
+import java.util.Stack;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("main method of test class");
+//		System.out.println("main method of test class");
+		Stack<String> stack = new Stack<>();
+		stack.push("1");
+		stack.push("2");
+		stack.push("3");
+		System.out.println(stack.peek());
+		System.out.println(stack);
+//		System.out.println(stack.firstElement());
+//		System.out.println(stack.lastElement());
+//		System.out.println(stack);
+//		System.out.println(stack.pop());
+//		System.out.println(stack);
 	}
 //	public void method1(){
 //		System.out.println("method1 of test class");
@@ -11,6 +23,7 @@ public class Test {
 	
 	public void method1(){
 		System.out.println("method1 of test class");
+		System.out.println("test");
 	}
 	
 	public void method2(){
