80f0df18b49eb3524aa78429d20a1cbc1a0bcfaa
zengjinc <102985588@qq.com>
2018-01-03 10:22:31 +0800

    update code

--- src/test/algorithm/Test.java
+++ src/test/algorithm/Test.java
@@ -9,25 +9,20 @@ public class Test {
 		stack.push("1");
 		stack.push("2");
 		stack.push("3");
-		System.out.println(stack.peek());
-		System.out.println(stack);
-//		System.out.println(stack.firstElement());
-//		System.out.println(stack.lastElement());
-//		System.out.println(stack);
-//		System.out.println(stack.pop());
-//		System.out.println(stack);
-	}
-//	public void method1(){
-//		System.out.println("method1 of test class");
-//	}
-	
-	public void method1(){
-		System.out.println("method1 of test class");
-		System.out.println("test");
+		
+		System.out.println("stack : " + stack);
+		
+//		System.out.println("pop : " + stack.pop());
+//		System.out.println("stack : " + stack);
+		
+//		System.out.println("peek : " + stack.peek());
+//		System.out.println("stack : " + stack);
+		
+//		System.out.println("firstElement : " + stack.firstElement());
+//		System.out.println("lastElement : " + stack.lastElement());
+//		System.out.println("stack : " + stack);
+		
 	}
 	
-	public void method2(){
-		System.out.println("methdo2 of test class");
-	}
 	
 }
